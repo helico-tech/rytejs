f9d106e544fb8c78d2c29e89703d26b60178355b
Arjan van Wieringen <info@helico-tech.nl>
2026-03-20 20:34:17 +0000

    refactor: reduce executor exports

--- packages/core/src/executor/index.ts
+++ packages/core/src/executor/index.ts
@@ -1,16 +1,7 @@
 export { WorkflowExecutor } from "./executor.js";
-export type { ExecutorPlugin } from "./plugin.js";
-export { defineExecutorPlugin, isExecutorPlugin } from "./plugin.js";
 export type {
-	BroadcastMessage,
-	CreateContext,
-	ExecuteContext,
 	ExecutionResult,
 	ExecutorContext,
-	ExecutorContextBase,
 	ExecutorError,
 	ExecutorMiddleware,
-	SubscriberRegistry,
 } from "./types.js";
-export { createSubscriberRegistry, withBroadcast } from "./with-broadcast.js";
-export { withStore } from "./with-store.js";
